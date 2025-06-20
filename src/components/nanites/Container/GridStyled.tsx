f5e0db1bfb858ece03e39c248a521c1d8c599049
Benjamin <benjamindeltour22@gmail.com>
2025-06-20 15:25:58 +0200

    feat: calendar is nice

--- src/components/nanites/Container/GridStyled.tsx
+++ src/components/nanites/Container/GridStyled.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
+import { CssProps } from '../../../types/CssProps.type';
 
-const GridStyled = styled.div`
+const GridStyled = styled.div<CssProps>`
   display: grid;
+  grid: ${(props) => props.grid ?? ""};
+  grid-template-columns: ${(props) => props.gridTemplateColumn ?? ""};
+  position: ${(props) => props.position ?? ""};
+  width: ${(props) => props.width ?? ""};
 `;
 
 export default GridStyled
